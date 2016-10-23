2ddd7e168eaee69e07a01e08303499b634b80168
sjclindsay <skip@sjclindsay.com>
2016-10-23 11:42:18 -0700

    Added Color Sensor to Servo Op MOde

--- FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ServoOp.java
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ServoOp.java
@@ -29,16 +29,27 @@ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package org.firstinspires.ftc.robotcontroller.external.samples;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.ftcrobotcontroller.R;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.ftccommon.DbgLog;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
+
+import org.firstinspires.ftc.robotcore.external.Func;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -46,19 +57,32 @@ import java.util.Date;
  * TeleOp Mode
  * <p>
  *Enables control of the robot via the gamepad
+
  */
+
 
 @TeleOp(name="WeCo: ServoOp", group="WeCo")
 //@Disabled
+
 public class ServoOp extends OpMode {
     Servo servoLeftRight;
     Servo servoPushButton;
+    Servo servoUpDown;
+    private static double SERVOLEFTRIGHT_STARTPOSITION = 0.5;
+    private static double SERVOUPDOWN_STARTPOSITION = 0.8;
+    private static double SERVOPUSHBUTTON_STARTPOSITION = 0.0;
+    // bPrevState and bCurrState represent the previous and current state of the button.
+    private static boolean bPrevState = false;
+    private static boolean bCurrState = false;
+    // bLedOn represents the state of the LED.
+    private boolean bLedOn = true;
 
 
     final static double servoMinRange = 0.0 ;
     final static double servoMaxRange = 1.0 ;
     double servoDelta = 0.25 ;
     double servoLeftRightPosition;
+    double servoUpDownPosition;
     double servoPositionPushButon;
     boolean BbuttonOn= false;
     ElapsedTime BbuttonTimmer = new ElapsedTime();
@@ -66,76 +90,141 @@ public class ServoOp extends OpMode {
     ElapsedTime AbuttonTimmer = new ElapsedTime();
     double buttonResetTime = 0.25 ;
 
+    ColorSensor sensorRGB;
+    DeviceInterfaceModule cdim;
+
+    // we assume that the LED pin of the RGB sensor is connected to
+    // digital port 5 (zero indexed).
+    static final int LED_CHANNEL = 5;
+
+    // hsvValues is an array that will hold the hue, saturation, and value information.
+    float hsvValues[] = {0F,0F,0F};
+
+    // values is a reference to the hsvValues array.
+    final float values[] = hsvValues;
 
     @Override
   public void init() {
-        servoLeftRight = hardwareMap.servo.get("servo_1") ;
-        servoPushButton = hardwareMap.servo.get("servo_2");
+        servoLeftRight = hardwareMap.servo.get("servoLeftRightP1") ;
+        servoUpDown = hardwareMap.servo.get("servoUpDownP2");
+        servoPushButton = hardwareMap.servo.get("servoButtonP3");
 
-  }
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F,0F,0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(R.id.RelativeLayout);
+
+        // get a reference to our DeviceInterfaceModule object.
+        cdim = hardwareMap.deviceInterfaceModule.get("dim");
+
+        // set the digital channel to output mode.
+        // remember, the Adafruit sensor is actually two devices.
+        // It's an I2C sensor and it's also an LED that can be turned on or off.
+        cdim.setDigitalChannelMode(LED_CHANNEL, DigitalChannelController.Mode.OUTPUT);
+
+        // get a reference to our ColorSensor object.
+        sensorRGB = hardwareMap.colorSensor.get("sensor_color");
+
+        // turn the LED on in the beginning, just so user will know that the sensor is active.
+        cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
+    }
 
     public void start() {
-        servoLeftRightPosition = 0.5 ;
-        servoPositionPushButon = 0.0;
+        servoLeftRightPosition = SERVOLEFTRIGHT_STARTPOSITION;
+        servoUpDownPosition = SERVOUPDOWN_STARTPOSITION;
+        servoPositionPushButon = SERVOPUSHBUTTON_STARTPOSITION;
 
     }
     
   @Override
   public void loop() {
 
-    gamepad2.right_stick_y;
+      setupLED(gamepad2.x);
 
-    if (gamepad2.a || gamepad2.b) {
+      // convert the RGB values to HSV values.
+      Color.RGBToHSV((sensorRGB.red() * 255) / 800, (sensorRGB.green() * 255) / 800, (sensorRGB.blue() * 255) / 800, hsvValues);
 
-        if (gamepad2.a && !AbuttonOn) {
+      if(gamepad2.right_trigger > 0.0) {
+          servoPositionPushButon += gamepad2.right_trigger/100;
+      } else {
+          servoPositionPushButon = 0.0;
+      }
 
-            servoPosition = servoPosition + servoDelta;
-            DbgLog.msg("=====Decrease arm position=====");
-            AbuttonOn = true;
-            AbuttonTimmer.reset();
+      servoLeftRightPosition += gamepad2.right_stick_x;
+      servoUpDownPosition += -gamepad2.right_stick_y;
+      servoPositionPushButon = Range.clip(servoPositionPushButon, servoMinRange, servoMaxRange) ;
 
-        }
-        if (AbuttonOn == true && (AbuttonTimmer.time() > buttonResetTime)) {
-            AbuttonOn = false;
-            DbgLog.msg("=====Reset AbuttonOn=====");
-        }
+      servoLeftRight.setPosition(servoLeftRightPosition);
+      servoUpDown.setPosition(servoUpDownPosition);
+      servoPushButton.setPosition(servoPositionPushButon);
 
+      telemetry.addLine()
+              .addData(" Right Trigger", new Func<String>() {
+                  @Override
+                  public String value() {
+                      return String.format("%.2f", gamepad2.right_trigger);
+                  }
+              });
+      telemetry.addLine()
+              .addData("Stick_x", new Func<String>() {
+                  @Override
+                  public String value() {
+                      return String.format("%.2f", gamepad2.right_stick_x);
+                  }
+              })
+              .addData("Stick_y", new Func<String>() {
+                  @Override
+                  public String value() {
+                      return String.format("%.2f", gamepad2.right_stick_y);
+                  }
+              });
+      telemetry.addLine()
+              .addData("Servo LT/RT", new Func<String>() {
+                  @Override
+                  public String value() {
+                      return String.format("%.2f", servoLeftRightPosition);
+                  }
+              })
+              .addData("Servo Up/Dn", new Func<String>() {
+                  @Override
+                  public String value() {
+                      return String.format("%.2f", servoUpDownPosition);
+                  }
+              });
+      telemetry.addLine()
+              .addData("LED", bLedOn ? "On" : "Off")
+              .addData("Hue", hsvValues[0]);
+      telemetry.addLine()
+              .addData("Red  ", sensorRGB.red())
+              .addData("Green", sensorRGB.green());
+      telemetry.addLine()
+              .addData("Blue ", sensorRGB.blue())
+              .addData("Clear", sensorRGB.alpha());
+      ;
+      telemetry.update();
 
-        //DbgLog.msg("=====servoPosition====="+String.format("%f", servoPositionClipped)) ;
-
-        if (gamepad2.b && !BbuttonOn) {
-
-            servoPosition = servoPosition - servoDelta;
-            DbgLog.msg("=====Decrease arm position=====" + String.format("%f", servoPosition));
-            BbuttonOn = true;
-            BbuttonTimmer.reset();
-
-        }
-        if (BbuttonOn == true && (BbuttonTimmer.time() > buttonResetTime)) {
-            BbuttonOn = false;
-            DbgLog.msg("=====Reset BbuttonOn=====");
-        }
-
-
-        if ((gamepad2.a == false) && (gamepad2.b == false)) {
-            DbgLog.msg("=====Not Pressed=====");
-        }
-    } else if ((gamepad2.left_trigger > 0) || (gamepad2.right_trigger > 0)) {
-            servoPosition = servoPosition + (gamepad2.left_trigger / 100) - (gamepad2.right_trigger / 100);
-    }
-
-    servoPosition = Range.clip(servoPosition, servoMinRange, servoMaxRange) ;
-
-    servo1.setPosition(servoPosition);
-
-    telemetry.addData("Left Trigger", "Left Trigger is at " + String.format("%.2f", gamepad2.left_trigger));
-    telemetry.addData("Right Trigger", "Right Trigger is at " + String.format("%.2f", gamepad2.right_trigger));
-    telemetry.addData("Servo Position", "Servo is at " + String.format("%f", servoPosition)) ;
 
   }
   @Override
   public void stop() {
 
   }
+    void setupLED(boolean buttonState) {
+
+        // check for button-press state transitions.
+        if ((bCurrState == true) && (bCurrState != bPrevState))  {
+
+            // button is transitioning to a pressed state. Toggle the LED.
+            bLedOn = !bLedOn;
+            cdim.setDigitalChannelState(LED_CHANNEL, bLedOn);
+        }
+        // update previous state variable.
+        bPrevState = bCurrState;
+    }
 }
 
