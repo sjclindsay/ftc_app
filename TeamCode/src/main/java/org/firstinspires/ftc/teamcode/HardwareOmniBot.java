818d06b41143a12c695886cd6b781bf502878f8c
sjclindsay <skip@sjclindsay.com>
2017-08-22 14:28:17 -0700

    Added IMU to Omni

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareOmniBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareOmniBot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -33,6 +35,10 @@ public class HardwareOmniBot
     public DcMotor  armMotor    = null;
     public Servo    leftClaw    = null;
     public Servo    rightClaw   = null;
+    public ColorSensor colorSensor = null;
+    public BNO055IMU imu = null;
+    public BNO055IMU.Parameters parameters = null;
+
 
     public static final double MID_SERVO       =  0.5 ;
     public static final double ARM_UP_POWER    =  0.45 ;
@@ -60,6 +66,25 @@ public class HardwareOmniBot
         armMotor    = hwMap.dcMotor.get("left_arm");
         Motor10.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
         Motor11.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+        colorSensor = hwMap.colorSensor.get("colorSensor1");
+
+        // Set up the parameters with which we will use our IMU. Note that integration
+        // algorithm here just reports accelerations to the logcat log; it doesn't actually
+        // provide positional information.
+        parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        // parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu".
+        imu = hwMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
 
         // Set all motors to zero power
         Motor00.setPower(0);
